643943ff4f827a57de61d820a037a4e2261f7ae4
Monge1h <jlo4321jl@gmail.com>
2023-12-12 09:49:12 -0600

    Update database configuration

--- database/database.js
+++ database/database.js
@@ -1,3 +1,6 @@
 import Sequelize from "@sequelize/core";
 
-export const sequelize = new Sequelize('sqlite::memory:')
+export const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+});
