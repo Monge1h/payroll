244068367d9e1f9d43887dd48aeb8522382df43b
Monge1h <jlo4321jl@gmail.com>
2023-12-12 10:56:33 -0600

    Fix import statement and remove commented code

--- controllers/employee/createEmployee.js
+++ controllers/employee/createEmployee.js
@@ -1,9 +1,5 @@
 import { Employee } from '../../models/Employee.js'
 
-// country: DataTypes.STRING,
-// name: DataTypes.STRING,
-// 	base_salary: DataTypes.REAL,
-// 	currency: DataTypes.STRING
 export const createEmployee = async (req, res) =>{
 	const { country, name, base_salary, currency } = req.body
 	const employe = await Employee.create({
